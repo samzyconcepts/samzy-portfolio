3459b6e04bd1d0b663b25306f5f045661dead0b6
samzyconcepts <samson.oderinwale@kibo.school>
2024-01-30 23:22:38 +0100

    adding the footer section

---
+++ src/components/Footer.jsx
@@ -0,0 +1,36 @@
+import StarSVG from "./StarSVG";
+
+const Footer = () => {
+    return (
+        <footer className="container">
+            <div className="flex justify-between py-4">
+                <div className="flex gap-2">
+                    <StarSVG />
+                    <h4>&copy; Cre8veSam 2023</h4>
+                </div>
+
+                <ul className="flex items-center gap-4">
+                    <li>
+                        <a href="https://x.com/cre8veSam" rel="noreferrer" target="_blank">
+                            Twitter
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="https://www.linkedin.com/in/samsonoderinwale/"
+                            rel="noreferrer"
+                            target="_blank">
+                            LinkedIn
+                        </a>
+                    </li>
+                    <li>
+                        <a href="mailto:oderinwalesamson1@gmail.com?cc=samzyolufemi@gmail.com">
+                            Mail
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    );
+};
+export default Footer;
