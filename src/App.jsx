eae0bef0daff89f0a394b2482b4226f436ba76c4
samzyconcepts <samson.oderinwale@kibo.school>
2024-02-09 15:50:49 +0100

    add aos library to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { useEffect } from "react";
+
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Work from "./components/Work";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
+import Aos from "aos";
+import "aos/dist/aos.css";
+
 function App() {
+    useEffect(() => {
+        Aos.init({});
+    });
+
     return (
         <>
             <Navbar />
